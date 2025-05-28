b3cf6bc972e7cceb88518e04c60a4f070218dfa4
Shema486 <137706925+Shema486@users.noreply.github.com>
2025-05-28 17:26:21 +0200

    Add solution - CodeHub

---
+++ 7-kyu/ones-and-zeros/ones-and-zeros.js
@@ -0,0 +1,12 @@
+const binaryArrayToNumber = arr => {
+  // your code
+  return parseInt(arr.join(""),2)
+};
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
