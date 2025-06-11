cf6b5d463b52c8ed38d98e2c57df1a7617a81bce
Shema486 <137706925+Shema486@users.noreply.github.com>
2025-06-11 11:01:32 +0200

    Update solution - CodeHub

--- 8-kyu/regular-ball-super-ball/regular-ball-super-ball.js
+++ 8-kyu/regular-ball-super-ball/regular-ball-super-ball.js
@@ -1,11 +1,5 @@
-var Ball = function (ballType) {
+var Ball = function(ballType) {
   // your code goes here
- this.ballType = ballType || "regular";
+  this.ballType = ballType || 'regular'
+  
 };
-​
-let ball1 = new Ball();
-let ball2 = new Ball("super");
-​
-console.log(ball1.ballType, "regular"); //=> "regular"
-console.log(ball2.ballType, "super");
-​
