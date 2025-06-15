6c5bb95372aa505b3c7bd0652ff1efad04e1f216
Shema486 <137706925+Shema486@users.noreply.github.com>
2025-06-15 13:19:13 +0200

    Update solution - CodeHub

--- 8-kyu/regular-ball-super-ball/regular-ball-super-ball.js
+++ 8-kyu/regular-ball-super-ball/regular-ball-super-ball.js
@@ -1,5 +1,21 @@
-var Ball = function(ballType) {
+​
+var Ball =function(ballType) {
   // your code goes here
-  this.ballType = ballType || 'regular'
-  
+ this.ballType = ballType || "regular"
 };
+​
+// var Ball = function (ballType) {
+//   // your code goes here
+//  this.ballType = ballType || "regular";
+// };
+​
+// let ball1 = new Ball();
+// let ball2 = new Ball("super");
+​
+// console.log(ball1.ballType, "regular"); //=> "regular"
+// console.log(ball2.ballType, "super");
+// var Ball = function(ballType) {
+//   // your code goes here
+//   this.ballType = ballType || 'regular'
+  
+// };
