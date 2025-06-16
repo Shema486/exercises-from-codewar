e9117e625af27c54ba3699679d0d2278f93aa256
Shema486 <137706925+Shema486@users.noreply.github.com>
2025-06-16 10:19:19 +0200

    Add solution - CodeHub

---
+++ 8-kyu/neutralisation/neutralisation.js
@@ -0,0 +1,18 @@
+function neutralise(s1, s2) {
+  // Here be dragons!
+  let result = ""
+  for(let i =0 ; i<s1.length;i++){
+   
+    if(s1[i] == "+" && s2[i] == "+"){
+       result+= "+"
+      }
+    else if(s1[i] == "-" && s2[i] == "-"){
+       result+="-"
+      }
+     else{
+       result+="0"
+     }
+  }
+ return result
+}
+ console.log( neutralise("+-+", "+--"))
