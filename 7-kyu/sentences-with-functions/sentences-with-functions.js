3fe8058647531f5aca15faebd67d897375303caa
Shema486 <137706925+Shema486@users.noreply.github.com>
2025-06-25 11:59:45 +0200

    Add solution - CodeHub

---
+++ 7-kyu/sentences-with-functions/sentences-with-functions.js
@@ -0,0 +1,40 @@
+function Adam(arg) {
+  return arg ? "Adam" +arg:  " Adam.";
+}
+​
+function has(arg) {
+  return " has " + arg;
+}
+​
+function a(arg) {
+  return "a " + arg;
+}
+​
+function dog(arg) {
+  return arg ? " dog" + arg : "dog.";
+}
+​
+function The(arg) {
+  return "The" + arg;
+}
+​
+function name(arg) {
+  return " name" + arg;
+}
+​
+function of(arg) {
+  return " of" + arg;
+}
+​
+function the(arg) {
+  return " the" + arg;
+}
+​
+function is(arg) {
+  return " is" + arg;
+}
+​
+function also(arg) {
+  return " also" + arg;
+}
+​
